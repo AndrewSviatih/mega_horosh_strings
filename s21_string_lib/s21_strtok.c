e5de5a1a8ec3c164e27def85d44f214c3c6fcae3
Kara Jude <karajude@le-a1.nov.21-school.ru>
2024-01-10 18:29:10 +0300

    basic final

--- s21_string_lib/s21_strtok.c
+++ s21_string_lib/s21_strtok.c
@@ -1,15 +1,5 @@
 #include "s21_string.h"
 
-// char *s21_strchr(const char *str, int c) {
-//     while (*str != '\0') {
-//         if (*str == c) {
-//             return (char *)str;
-//         }
-//         str++;
-//     }
-//     return NULL;
-// }
-
 char *s21_strtok(char *str, const char *delim) {
     static char *token = NULL;
 
@@ -41,39 +31,3 @@ char *s21_strtok(char *str, const char *delim) {
 
     return start_ptr;
 }
-
-// size_t s21_strlen(const char *str) {
-//     size_t length = 0;
-//     while (*str != '\0') {
-//         length++;
-//         str++;
-//     }
-//     return length;
-// }
-
-// int main() {
-//     char str1[] = "Aboba++Floppa_! Kotya====!Shleppa";
-//     char str2[] = "Aboba++Floppa_! Kotya====!Shleppa";
-//     const char delims[] = "+_! =";
-
-//     char *got = s21_strtok(str1, delims);
-//     char *expected = strtok(str2, delims);
-
-//     s21_strlen(got);
-//     s21_strlen(expected);
-
-
-//     while (got && expected) {
-//         got = s21_strtok(NULL, delims);
-//         expected = strtok(NULL, delims);
-
-//         if (got || expected) {
-//             s21_strlen(got);
-//             s21_strlen(expected);
-//         } else {
-//             ;
-//         }
-//     }
-
-//     return 0;
-// }
