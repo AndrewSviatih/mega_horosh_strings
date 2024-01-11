3aaa2eb902ff5eca78ff5bab5abefa5206729c2d
Kara Jude <karajude@bo-d5.nov.21-school.ru>
2024-01-11 22:11:04 +0300

    sprintf 0.4.5

--- s21_sprintf/s21_sprintf.c
+++ s21_sprintf/s21_sprintf.c
@@ -1,4 +1,5 @@
 #include "../s21_string_lib/s21_string.h"
+#include "string.h"
 
 typedef struct {
     int minus;
@@ -8,7 +9,7 @@ typedef struct {
     int zero;
     int width;
     int accurency;
-    char lenghth;
+    char length;
     int number_system;
     int flag_to_size;
     int dot;
@@ -36,6 +37,8 @@ const char *get_specs(const char *format, Spec *specs) {
     }
     specs->space = (specs->space && !specs->plus);
     specs->zero = (specs->zero && !specs->minus);
+
+    return format;
 }
 
 const char *get_width(const char *format, int *width, va_list *input){
@@ -51,14 +54,13 @@ const char *get_width(const char *format, int *width, va_list *input){
         if ('0' <= *format && *format <= '9'){
             *width *= 10;
             *width += *format - '0'; 
-        } else
+        } else {
             break;
-        
+        }
         format++;
     }
 
     return format;
-
 }
 // "%+-014.6hd" парсим этого монстра
 const char *set_specs(Spec *specs, const char *format, va_list *input) {
@@ -74,13 +76,13 @@ const char *set_specs(Spec *specs, const char *format, va_list *input) {
     }
 // для разных даблов
     if (*format == 'L')
-        specs->lenghth = 'L';
+        specs->length = 'L';
     else if (*format == 'l')
-        specs->lenghth = 'l';
+        specs->length = 'l';
     else if (*format == 'h') 
-        specs->lenghth = 'h';
+        specs->length = 'h';
 
-    if (specs->lenghth != 0) format += 1;
+    if (specs->length != 0) format += 1;
 
 // ширина не может быть отрицательной
     if (specs->width < 0) {
@@ -91,10 +93,48 @@ const char *set_specs(Spec *specs, const char *format, va_list *input) {
     return format;
 }
 
+size_t get_size_to_decimal(Spec *specs, long int num){
+
+    size_t res = 0;
+
+    long int copy_num = num;
+
+    if (copy_num < 0) copy_num = -copy_num;
+
+    while (copy_num > 0) {
+        copy_num /= 10;
+        res++;
+    }
+
+    if (copy_num == 0 && res == 0 && (specs->accurency || specs->width || specs->space)) {
+        res++;
+    }
+
+    
+    
+}
+
+char *print_decimal(char *res, Spec specs, va_list *input){
+
+    long int num = 0;
+
+    if (specs.length == 'l') {
+        num = (long int)va_arg(*input, long int);
+    } else if (specs.length == 'h') {
+        num = (short)va_arg(*input, short);
+    } else {
+        num = (int)va_arg(*input, int);
+    }
+
+    size_t size_to_decimal = get_size_to_decimal(&specs, num);
+
+    char *str_to_num = malloc(sizeof(char) * size_to_decimal);
+}
+
 char *parser(char *res, char *res_begining, const char *format, Spec specs, va_list *input){
 
     if (*format == 'd' || *format == 'd') {
-        str = printf_decimal()
+        res = printf_decimal();
     }
 
 }
@@ -103,7 +143,7 @@ int s21_sprintf(char *res, const char *format, ...){
 
     char specifiers[] = "diuoxXcsnpfFeEgG%";
 
-    char *res_begining = res;
+    char *start = res;
 
     va_list input = {0};
     va_start(input, format);
@@ -114,16 +154,27 @@ int s21_sprintf(char *res, const char *format, ...){
             Spec specs = {0};
             specs.number_system = 10;
             format = set_specs(&specs, format, &input);
-            while (!s21_strchr(specifiers, *format)) format++;
-            res = parser();
+            while (!strchr(specifiers, *format)) format++;
+            // res = parser();
         } else {
             *res = *format;
             res++;
         }
+        format++;
     }
 
 
     va_end(input);
 
-    return res - res_begining;
+    return res - start;
+}
+
+int main() {
+
+    char format[256] = "%+-014.6hd adsdsa: %ld dsaads: %s %x";
+    char str[256];
+
+    int res = s21_sprintf(str, format, 24, 241, "dsaas", 1);
+    printf("%d", res);
+
 }
