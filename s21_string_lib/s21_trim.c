b4d887a6d5dec95d33d74456b67f4f95f044b86f
Kara Jude <karajude@le-c7.nov.21-school.ru>
2024-02-11 18:03:25 +0300

    final

--- s21_string_lib/s21_trim.c
+++ s21_string_lib/s21_trim.c
@@ -9,19 +9,16 @@ void *s21_trim(const char *src, const char *trim_chars) {
   if (src == S21_NULL) {
     result = S21_NULL;
   } else {
-
     s21_size_t start_index = 0;
     s21_size_t end_index = s21_strlen(src) - 1;
     s21_size_t src_length = s21_strlen(src);
     while (start_index < src_length &&
-           s21_strchr(trim_chars, src[start_index]) != S21_NULL)
-    {
+           s21_strchr(trim_chars, src[start_index]) != S21_NULL) {
       start_index++;
     }
 
     while (end_index >= start_index &&
-           s21_strchr(trim_chars, src[end_index]) != S21_NULL)
-    {
+           s21_strchr(trim_chars, src[end_index]) != S21_NULL) {
       end_index--;
     }
     result = calloc(end_index - start_index + 1, sizeof(char));
