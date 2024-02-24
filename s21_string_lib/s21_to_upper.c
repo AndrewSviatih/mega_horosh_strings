7045b7885e8d8e4e32c5ee7cded365dfd207666a
Kirill <playnikov@gmail.com>
2024-02-24 13:14:25 +0300

    edit library string

--- s21_string_lib/s21_to_upper.c
+++ s21_string_lib/s21_to_upper.c
@@ -7,17 +7,16 @@ void *s21_to_upper(const char *str) {
     result = S21_NULL;
   } else {
     result = calloc(s21_strlen(str) + 1, sizeof(char));
-    int flag = 1;
-    for (s21_size_t i = 0; i < s21_strlen(str) && flag == 1; i++) {
-      if (str[i] >= 'a' && str[i] <= 'z') {
-        result[i] = str[i] - 'a' + 'A';
-      } else if (str[i] >= 0 && str[i] < 127) {
-        result[i] = str[i];
-      } else {
-        flag = 0;
-        free(result);
-        result = S21_NULL;
+    if (result) {
+      for (s21_size_t i = 0; i < s21_strlen(str); i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+          result[i] = str[i] - 'a' + 'A';
+        } else {
+          result[i] = str[i];
+        }
       }
+    } else {
+      result = S21_NULL;
     }
   }
 
